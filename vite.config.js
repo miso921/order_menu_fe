4778c0a369ff7efd4c28f6bcb6452dc73e58dd71
sang <tgu94@naverr.com>
2025-01-16 09:49:43 +0900

    기본 셋팅

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // 백엔드 서버 URL
+        changeOrigin: true, // 대상 서버의 CORS 정책 우회
+        rewrite: (path) => path.replace(/^\/api/, ""), // '/api'를 제거
+      },
+    },
+  },
+
+});
